15bde56e28817ec8bcbdde736794cccc1e0bd4a6
mhlee1215 <mhlee1215@gmail.com>
2015-06-24 14:55:29 -0700

    Initial Commit

---
+++ MusicHubiOS_oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicHubiOS_oc
+//
+//  Created by Minhaeng Lee on 6/24/15.
+//  Copyright (c) 2015 Minhaeng Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
